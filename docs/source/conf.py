595c72d68f86f8dd398b3b82fd2cacf13bfd390a
albert-5 <981287080@qq.com>
2022-05-01 19:42:08 +0800

    主题测试

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,7 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark','sphinx_markdown_tables'] 
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,8 +53,8 @@ extensions = ['recommonmark','sphinx_markdown_tables']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = 'furo'
+html_theme = 'alabaster'
+# html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
